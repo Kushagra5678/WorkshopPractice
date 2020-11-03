4e8f77a47477695e35e2ff870dadfe5b67473aa8
Kushagra5678 <kushu.7feb@gmail.com>
2020-11-03 23:46:37 +0530

    Contact created

--- src/main/java/com/practice/addressbook/AddressBookMain.java
+++ src/main/java/com/practice/addressbook/AddressBookMain.java
@@ -5,6 +5,8 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to address Book system");
+		Contacts contact = new Contacts();
 	}
+	
 
 }
